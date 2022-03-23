e4f275fc09cacb2dadbd3cb550cfafe8ffad3258
manhunter7-2 <thomas.piquet7@gmail.com>
2022-03-23 18:13:44 +0100

    save 23/03-18.13

--- core/src/com/mygdx/game/Tools/Background.java
+++ core/src/com/mygdx/game/Tools/Background.java
@@ -8,14 +8,21 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Background {
-    public void show(){
         OrthographicCamera camera = new OrthographicCamera(1, Gdx.graphics.getHeight()/Gdx.graphics.getWidth());
         SpriteBatch backBatch = new SpriteBatch();
         Texture tex = new Texture(Gdx.files.internal("backgrounds/Blue Nebula 1 - 1024x1024.png"));
         TextureRegion texR = new TextureRegion(tex, 0, 0, 1024, 1024);
         Sprite sp = new Sprite(texR);
-        sp.setSize(1.0f, 1.0f);
-        sp.setOrigin(sp.getWidth()/2, sp.getHeight()/2);
-        sp.setPosition(-sp.getWidth()/2, -sp.getHeight()/2);
+
+        public void setSprite() {
+            sp.setSize(1.0f, 1.0f);
+            sp.setOrigin(sp.getWidth() / 2, sp.getHeight() / 2);
+            sp.setPosition(-sp.getWidth() / 2, -sp.getHeight() / 2);
+        }
+    public void render(){
+        backBatch.setProjectionMatrix(camera.combined);
+        backBatch.begin();
+        sp.draw(backBatch);
+        backBatch.end();
     }
 }
